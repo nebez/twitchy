313a22cb8de0a339338f00df27faeb53dea50eaa
Nebez Briefkani <me@nebezb.com>
2014-09-07 20:49:08 -0400

    sub-tabs

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,9 @@ function TwitchyViewModel() {
 	// Members
 	var self = this;
 	self.tabs = ['Search', 'Channels', 'Games'];
+	self.subTabs = ko.observable();
 	self.currentTab = ko.observable();
+	self.currentSubTab = ko.observable();
 	self.currentStream = ko.observable();
 	self.streamList = ko.observable();
 
@@ -12,6 +14,10 @@ function TwitchyViewModel() {
 	self.goToTab = function(tab) {
 		// Navigate to the named tab
 		window.location.hash = tab;
+	};
+
+	self.goToSubTab = function(subTab) {
+		window.location.hash = self.currentTab() + '/' + subTab;
 	};
 
 	// Generates the css necessary for tabbed navigation
@@ -26,24 +32,44 @@ function TwitchyViewModel() {
 		}
 	};
 
+	self.subNavLinkCss = function(tabName) {
+		if(tabName === self.currentSubTab())
+		{
+			return 'active tab-1-5';
+		}
+		else
+		{
+			return 'tab-1-5';
+		}
+	};
+
 	// Hash Router
 	var router = new Sammy(function() {
-		// Tab routes
+		// Search routes
 		this.get('#Search', function() {
 			self.currentTab('Search');
-			self.currentStream(null);
-		});
-		
-		// Tab routes
-		this.get('#Channels', function() {
-			self.currentTab('Channels');
+			self.subTabs(['Channels', 'Streams', 'Games']);
+			self.currentSubTab('Channels');
 			self.currentStream(null);
 		});
 
-		// Tab routes
+		// Channel routes
+		this.get('#Channels', function() {
+			self.currentTab('Channels');
+			self.subTabs(['Favorites', 'Featured', 'Popular']);
+			self.currentSubTab('Favorites');
+			self.currentStream(null);
+		});
+
+		// Game routes
 		this.get('#Games', function() {
+			self.subTabs(null);
 			self.currentTab('Games');
 			self.currentStream(null);
+		});
+
+		this.get('#Games/:name', function() {
+
 		});
 
 		// Default route
