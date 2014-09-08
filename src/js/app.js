bb50917e85db960dd589ca3045f1f40cd40fe3cb
Nebez Briefkani <me@nebezb.com>
2014-09-07 20:08:52 -0400

    twitchy

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,64 @@
 /* jshint strict: false */
-/* global window,ko,$ */
+/* global window,ko,Sammy */
 function TwitchyViewModel() {
 	// Members
 	var self = this;
+	self.tabs = ['Search', 'Channels', 'Games'];
+	self.currentTab = ko.observable();
+	self.currentStream = ko.observable();
 	self.streamList = ko.observable();
 
 	// Behaviours
+	self.goToTab = function(tab) {
+		// Navigate to the named tab
+		window.location.hash = tab;
+	};
+
+	// Generates the css necessary for tabbed navigation
+	self.navLinkCss = function(tabName, index) {
+		if(tabName === self.currentTab())
+		{
+			return 'active link item-' + index;
+		}
+		else
+		{
+			return 'link item-' + index;
+		}
+	};
+
+	// Hash Router
+	var router = new Sammy(function() {
+		// Tab routes
+		this.get('#Search', function() {
+			self.currentTab('Search');
+			self.currentStream(null);
+		});
+		
+		// Tab routes
+		this.get('#Channels', function() {
+			self.currentTab('Channels');
+			self.currentStream(null);
+		});
+
+		// Tab routes
+		this.get('#Games', function() {
+			self.currentTab('Games');
+			self.currentStream(null);
+		});
+
+		// Default route
+		this.get('', function() {
+			this.app.runRoute('get', '#Channels');
+		});
+	});
+
+	// Run the router
+	router.run();
+}
+
+ko.applyBindings(new TwitchyViewModel());
+
+/*
 	self.openStream = function(stream) { window.location.hash = stream.name; };
 	self.listStreams = function() {
 		$.ajax({
@@ -15,10 +68,9 @@ function TwitchyViewModel() {
 		})
 		.done(function(response) {
 			self.streamList(response);
+			console.log(response);
 		});
 	};
 
 	self.listStreams();
-}
-
-ko.applyBindings(new TwitchyViewModel());
+ */
